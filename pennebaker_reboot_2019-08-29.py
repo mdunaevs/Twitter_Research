87817a3fc0252f7c2d6b042ecd44022876e2d94b
Max <mdunaevs@andrew.cmu.edu>
2019-09-18 21:49:39 -0400

    added counts for emojis

--- pennebaker_reboot_2019-08-29.py
+++ pennebaker_reboot_2019-08-29.py
@@ -10,6 +10,8 @@ import xlrd
 import json
 import twitter_col
 import pandas as pd
+import emoji
+
 
 
 # In[2]:
@@ -35,13 +37,13 @@ def readFile(filepath, inFile = 'JSON'):
         df.columns = ['user_id', 'status_id', 'status_text']
         df['status_text'] = [x.lower() for x in df['status_text']]
         return(df)
-    
+
     if(inFile == 'CSV'):
         try:
             df = pd.read_csv(filepath, encoding = 'utf-8')
         except ValueError:
             df = pd.read_csv(filepath, encoding = 'ISO-8859-1')
-            
+
         if (df.shape[1] > 2):
             try:
                 df = df[['id_str', 'status_id', 'status_text']]
@@ -49,16 +51,16 @@ def readFile(filepath, inFile = 'JSON'):
                 df = df[['user_id', 'status_id', 'status_text']]
         else:
             df.columns = ['status_id', 'status_text']
-        
+
         df['status_text'] = [str(x).lower() for x in df['status_text']]
         return(df)
-    
+
     if(inFile == 'Reddit'):
         try:
             df = pd.read_csv(filepath, encoding = 'utf-8')
         except ValueError:
             df = pd.read_csv(filepath, encoding = 'ISO-8859-1')
-            
+
         df = df[['comment_id', 'comment_text']]
         df.columns = ['status_id', 'status_text']
         df['status_text'] = [x.lower() for x in df['status_text']]
@@ -69,7 +71,7 @@ def readFile(filepath, inFile = 'JSON'):
 
 
 # Get words
-categories = ['pronouns', 'absolutist', 'exclusive', 'abusive']
+categories = ['pronouns', 'absolutist', 'exclusive', 'abusive', 'emoji']
 
 def getWords(dictpath = './dicts', langs = ['English']):
     start = time.time()
@@ -77,11 +79,11 @@ def getWords(dictpath = './dicts', langs = ['English']):
     for cat in categories:
         print("Now doing word list for {}!".format(cat))
         wordlist[cat] = []
-        
+
         wb = xlrd.open_workbook('{}/dict_{}.xlsx'.format(dictpath, cat))
         sheet = wb.sheet_by_index(0)
         catCol = -1
-        
+
         for lang in langs:
             for i in range(sheet.ncols):
                 if(sheet.cell_value(0, i) == lang):
@@ -89,15 +91,15 @@ def getWords(dictpath = './dicts', langs = ['English']):
             for i in range(sheet.ncols):
                 if(sheet.cell_value(0, i) == "Category 1"):
                     catCol = i
-            
-            
+
+
             if (catCol >= 0):
                 for i in range(1, sheet.nrows):
                     wordlist[cat].append((sheet.cell_value(i, langCol), sheet.cell_value(i, catCol)))
             else:
                 for i in range(1, sheet.nrows):
                     wordlist[cat].append(sheet.cell_value(i, langCol))
-    
+
     for cat in categories:
         if (cat == 'pronouns'):
             wordlist[cat] = [(x.lower(), y) for (x, y) in wordlist[cat] if x != '']
@@ -105,13 +107,15 @@ def getWords(dictpath = './dicts', langs = ['English']):
             wordlist[cat] = [(x.lower(), y) for (x, y) in wordlist[cat] if x != '']
         else:
             wordlist[cat] = [x.lower() for x in wordlist[cat] if x != '']
-    
+
     print("Generated all word lists for {} languages in {:.2f} seconds!".format(len(langs), time.time() - start))
-        
+
     return wordlist
 
 
 # In[10]:
+
+
 
 
 # Count words
@@ -120,7 +124,7 @@ def getCounts(tweet, wordlist):
     start = time.time()
 
     rowvec = []
-    
+
     text = getThreeGrams(tweet)
 
     for cat in categories:
@@ -137,12 +141,25 @@ def getCounts(tweet, wordlist):
                 sublist = [x for (x, y) in wordlist[cat] if y == subcat]
                 count = sum([text.count(word) for word in sublist])
                 rowvec.append(count)
+        elif(cat == 'emoji'):
+            count = getEmojiCounts(text)
+            rowvec.append(count)
         else:
             count = sum([text.count(word) for word in wordlist[cat]])
             rowvec.append(count)
-    
+
     return rowvec
 
+
+
+
+def getEmojiCounts(text):
+
+    sublist = [x for x in text if x in emoji.UNICODE_EMOJI]
+
+    return len(sublist)
+
+print(getEmojiCounts('🤔 🙈 me así, bla es se 😌 ds 💕👭👙'))
 
 # In[11]:
 
@@ -168,27 +185,27 @@ def pennebake(filepath, inFile = 'JSON', dictpath = './dicts', langs = ['English
     df = readFile(filepath, inFile)
     print("Going to do a file with {} tweets!".format(df.shape[0]))
     wordlist = getWords(dictpath, langs)
-    
+
     i = 0
     for tweet in [x for x in df['status_text']]:
         #counter = counter.append(getCounts(tweet, wordlist = wordlist))
         counter.loc[i,:] = getCounts(tweet, wordlist = wordlist)
         i = i + 1
-        
+
         if(i % 1000 == 0):
             print('Now done with {} tweets out of {} in {:.2f} seconds!'.format(i, df.shape[0], time.time() - start))
-    
+
     if (inFile=='JSON'):
         counter['user_id'] = df['user_id']
-        counter['status_id'] = df['status_id']   
-        counter = counter[['user_id', 'status_id', 'first', 'second', 'third', 
+        counter['status_id'] = df['status_id']
+        counter = counter[['user_id', 'status_id', 'first', 'second', 'third',
                            'absolutist', 'non-absolutist', 'exclusive', 'abusive']]
     else:
         counter['status_id'] = df['status_id']
-        counter = counter[['status_id', 'first', 'second', 'third', 
+        counter = counter[['status_id', 'first', 'second', 'third',
                            'absolutist', 'non-absolutist', 'exclusive', 'abusive']]
     print('Finished word counts in {:.2f} seconds!'.format(time.time() - start))
-    
+
     counter['readability'] = [getReadability(x) for x in df['status_text']]
     counter.to_csv(filepath+'_pennebaked.csv', index = None)
     return(counter)
